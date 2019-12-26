414a991e00a898be64f939ed5ed275fea22110d6
KingAbesh <abeshekwere@gmail.com>
2019-12-26 15:27:35 +0100

    Event loop.

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Please enter a message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send<button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
+const routes = require("./routes");
 
-  if (url == "/message" && method === "POST") {
-    const body = [];
-    req.on("data", chunk => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-    });
-    fs.writeFileSync("message.txt", "Here is a message");
-    res.writeHead(302, {
-      Location: "/"
-    });
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<p>Hello World</p>");
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
