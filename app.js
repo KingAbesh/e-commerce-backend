198955df9f9599781c5133600320b433a186be6f
KingAbesh <abeshekwere@gmail.com>
2020-02-05 07:32:39 +0100

    Final Commit

--- app.js
+++ app.js
@@ -67,16 +67,16 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: "Abas",
-          email: "abas@test.com",
-          cart: { items: [] }
-        });
-        user.save();
-      }
-    });
+    // User.findOne().then(user => {
+    //   if (!user) {
+    //     const user = new User({
+    //       name: "Abas",
+    //       email: "abas@test.com",
+    //       cart: { items: [] }
+    //     });
+    //     user.save();
+    //   }
+    // });
     console.log("Database connected");
     app.listen(3000);
   })
